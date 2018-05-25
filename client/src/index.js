b949091c38d91cc1bb0ad2569525b9901ff56aa9
JFenderson <fenderson.joseph@gmail.com>
2018-05-25 15:32:32 -0500

    pushing work

--- client/src/index.js
+++ client/src/index.js
@@ -121,6 +121,27 @@ function numberTransition(id, endPoint, transitionDuration, transitionEase){
 
 //start slideshow
 
+function slideSwitch() {
+  var $active = $('#slideshow IMG.active');
+
+  if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+
+  var $next =  $active.next().length ? $active.next()
+      : $('#slideshow IMG:first');
+
+  $active.addClass('last-active');
+      
+  $next.css({opacity: 0.0})
+      .addClass('active')
+      .animate({opacity: 1.0}, 1000, function() {
+          $active.removeClass('active last-active');
+      });
+}
+
+$(function() {
+  setInterval( "slideSwitch()", 5000 );
+});
+
 //slideshow
 // var myIndex = 0;
 // carousel();
