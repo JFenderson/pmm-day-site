ef5649798eb76e942bd448c4ac583072051e27b3
JFenderson <fenderson.joseph@gmail.com>
2018-07-26 13:13:37 -0500

    editing the email features

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -30,9 +30,12 @@ router.post('/', (req, res) => {
     from: `fenderson.joseph@gmail.com`,// who the email is coming from..in the contact form
     to: `${name} <${email}>`,//who the email is going to
     subject: `Thank you for Signing Up to the PMM Weekend Site`,//subject line
-    html: `<div>
-    <h2>Hello ${name.firstName}, have been added to our PMM Database which will be used to contact you for future events currently in the works.</h2>
-    <p>If you do not wish to be contacted please repond to this email with a simple NO and you will be removed from the listing.</p>
+    html: `
+    <div style="text-align: center;">
+      <h1>Hello <span style="color: purple;">${name.firstName}</span>,</h1> <h2>Thank you signing up. You have been added to the PMM Database which will be used to contact you for future events such as road trips to support the band, band schedules and more currently in the works.</h2>
+      <h3>Our goal is to build and get every person that marched as PMM in our database so that we can have a directory. With your help we can get there so spread the word to sign up from the website.</h3>
+      <h1 style="color: purple"><span style="color: gold;">P</span>MM 1X!!!</h1>
+      <p>If you do not wish to be contacted please repond to this email saying <strong>"PLEASE REMOVE"</strong> and you will be removed from the listing.</p>
     </div>`,
 };
 
